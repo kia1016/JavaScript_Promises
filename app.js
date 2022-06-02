377e1c8f00262dd96d2862d6405ddccb9c0258f0
kia1016 <kia1016@gmail.com>
2022-06-01 21:42:28 -0400

    JS_Promises

--- app.js
+++ app.js
@@ -3,28 +3,28 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
-function watchTurorialCallback(callback, errorCallback) {
-  let userLeft = false;
-  let userWatchingLiveStream = true;
+// function watchTurorialCallback(callback, errorCallback) {
+//   let userLeft = false;
+//   let userWatchingLiveStream = true;
 
-  if (userLeft) {
-    errorCallback({
-      name: "User Left",
-      message: ":(",
-    });
-  } else if (userWatchingLiveStream) {
-    callback("Thumbs up and Subscribe");
-  }
-}
+//   if (userLeft) {
+//     errorCallback({
+//       name: "User Left",
+//       message: ":(",
+//     });
+//   } else if (userWatchingLiveStream) {
+//     callback("Thumbs up and Subscribe");
+//   }
+// }
 
-watchTurorialCallback(
-  (message) => {
-    console.log(message);
-  },
-  (error) => {
-    console.log(error.name + " " + error.message);
-  }
-);
+// watchTurorialCallback(
+//   (message) => {
+//     console.log(message);
+//   },
+//   (error) => {
+//     console.log(error.name + " " + error.message);
+//   }
+// );
 
 let watching = new Promise((resolve, reject) => {
   let userWatchingLiveStream = false;
